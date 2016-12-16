bd8fa929a307950ba00554f4e813219578096e22
Alexander Areshnikov <alex.areshnikov@plansource.com>
2016-12-15 17:03:15 -0700

    RNA-Transcription. Solved.

--- rna-transcription/lib/rna_transcription.rb
+++ rna-transcription/lib/rna_transcription.rb
@@ -3,5 +3,22 @@ module BookKeeping
 end
 
 class Complement
+  TRANSCRIPTION = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
 
+  def self.of_dna(dna)
+    rna = String.new
+    dna.each_char do |dna_strand|
+      rna_strand = TRANSCRIPTION[dna_strand]
+
+      rna = String.new and break unless rna_strand
+      rna << rna_strand
+    end
+
+    rna
+  end
 end
