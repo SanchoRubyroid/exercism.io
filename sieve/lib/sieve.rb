041688a83d47d5eeab0502303962b0afc290edb3
Alexander Areshnikov <alex.areshnikov@plansource.com>
2017-01-05 16:17:27 -0700

    Sieve. Solved.

--- sieve/lib/sieve.rb
+++ sieve/lib/sieve.rb
@@ -3,5 +3,28 @@ module BookKeeping
 end
 
 class Sieve
+  def initialize(limit)
+    @base_range = (2..limit).to_a
+    @primes = []
+  end
 
+  def primes
+    first_number = @base_range.shift
+    return @primes if first_number.to_i < 2
+
+    drop_multipliers(first_number, @base_range)
+
+    @primes
+  end
+
+  def drop_multipliers(prime, range)
+    @primes << prime
+    range_dropped_multipliers = range.reject { |test_number| test_number%prime == 0 }
+
+    if range_dropped_multipliers == range
+      @primes += range_dropped_multipliers
+    else
+      drop_multipliers(range_dropped_multipliers.shift, range_dropped_multipliers)
+    end
+  end
 end
