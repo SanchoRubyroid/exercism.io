aa4ef653a7441ff71033c9198d50140e74ad7de7
Alexander Areshnikov <alex.areshnikov@plansource.com>
2017-01-05 14:42:07 -0700

    Difference Of Squares. Solved.

--- difference-of-squares/lib/difference_of_squares.rb
+++ difference-of-squares/lib/difference_of_squares.rb
@@ -3,5 +3,21 @@ module BookKeeping
 end
 
 class Squares
+  attr_reader :limit
 
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def difference
+    return square_of_sum - sum_of_squares
+  end
+
+  def square_of_sum
+    (1..limit).inject(0) { |sum, number| sum + number } ** 2
+  end
+
+  def sum_of_squares
+    (1..limit).inject(0) { |sum, number| sum + number ** 2 }
+  end
 end
