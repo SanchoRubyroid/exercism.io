752610faf990766fe3608fb73131fb05a68c8b88
Alexander Areshnikov <alex.areshnikov@plansource.com>
2017-01-05 15:22:45 -0700

    Pangram. Solved.

--- pangram/lib/pangram.rb
+++ pangram/lib/pangram.rb
@@ -3,5 +3,9 @@ module BookKeeping
 end
 
 class Pangram
+  ASCII_ALPHABET = (97..122).to_a.freeze
 
+  def self.pangram?(phrase)
+    phrase.downcase.each_byte.select{|ascii_char| ASCII_ALPHABET.include?(ascii_char)}.uniq.sort == ASCII_ALPHABET
+  end
 end
--- pangram/test/pangram_test.rb
+++ pangram/test/pangram_test.rb
@@ -13,55 +13,55 @@ class PangramTest < Minitest::Test
   end
 
   def test_pangram_with_only_lower_case
-    skip
+    #skip
     phrase = 'the quick brown fox jumps over the lazy dog'
     assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
   end
 
   def test_missing_character_x
-    skip
+    #skip
     phrase = 'a quick movement of the enemy will jeopardize five gunboats'
     refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
   end
 
   def test_another_missing_character_x
-    skip
+    #skip
     phrase = 'the quick brown fish jumps over the lazy dog'
     refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
   end
 
   def test_pangram_with_underscores
-    skip
+    #skip
     phrase = 'the_quick_brown_fox_jumps_over_the_lazy_dog'
     assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
   end
 
   def test_pangram_with_numbers
-    skip
+    #skip
     phrase = 'the 1 quick brown fox jumps over the 2 lazy dogs'
     assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
   end
 
   def test_missing_letters_replaced_by_numbers
-    skip
+    #skip
     phrase = '7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog'
     refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
   end
 
   def test_pangram_with_mixed_case_and_punctuation
-    skip
+    #skip
     phrase = '"Five quacking Zephyrs jolt my wax bed."'
     assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
   end
 
   def test_pangram_with_non_ascii_characters
-    skip
+    #skip
     phrase = 'Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.'
     assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
   end
 
   def test_panagram_in_alphabet_other_than_ascii
-    skip
+    #skip
     phrase = 'Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства.'
     refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
   end
