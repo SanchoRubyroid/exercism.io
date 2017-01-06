dde9209602aa48ff7c10a613a4e7d68398cc4280
Alexander Areshnikov <alex.areshnikov@plansource.com>
2017-01-06 15:36:56 -0700

    Robot Name. Solved.

--- robot-name/lib/robot_name.rb
+++ robot-name/lib/robot_name.rb
@@ -3,7 +3,23 @@ module BookKeeping
 end
 
 class Robot
-  def name
-    # TODO
+  attr_reader :name
+
+  @@previous_names = []
+
+  def initialize
+    reset
+  end
+
+  def reset
+    generate_name
+    @@previous_names.include?(name) ? reset : (@@previous_names << name)
+  end
+
+  def generate_name
+    @name = String.new
+
+    2.times { @name << (rand(25) + 65).chr }
+    @name << (rand(899) + 100).to_s
   end
 end
