0e5adaa50810b7b1aca1e25f8c7f943029b914e2
Alexander Areshnikov <alex.areshnikov@plansource.com>
2017-01-06 16:29:20 -0700

    Nth Prime. Solved.

--- nth-prime/lib/nth_prime.rb
+++ nth-prime/lib/nth_prime.rb
@@ -3,7 +3,27 @@ module BookKeeping
 end
 
 class Prime
-  def self.nth
-    # TODO
+  def self.nth(target_prime_element_number)
+    raise ArgumentError if target_prime_element_number == 0
+    Prime.new(target_prime_element_number).prime
+  end
+
+  def initialize(target_prime_element_number)
+    @target_prime_element_number = target_prime_element_number
+    @primes = [2]
+    @counter = 2
+  end
+
+  def prime
+    while prime_not_found? do
+      @counter += 1
+      @primes << @counter unless @primes.any? { |prime| @counter % prime == 0 }
+    end
+
+    @primes.last
+  end
+
+  def prime_not_found?
+    @primes.size != @target_prime_element_number
   end
 end
